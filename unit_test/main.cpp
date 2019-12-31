d2e8c65b168dc93353ffb1f13a377b0d143e1b49
NudistKiwi <duslxx@gmx.at>
2019-12-31 18:06:09 +0100

    new file

---
+++ unit_test/main.cpp
@@ -0,0 +1,30 @@
+#include "library/csv_engine/include/csv_engine.h"
+int main(){
+int i,j;
+int k;
+
+//string all=ingest_xlsx(fi);
+
+char file[]="BA_DATA.csv";
+csv_file test(file);
+std::cout<<"cols  "<<test.Ncols()<<std::endl;
+std::cout<<"rows  "<<test.Nrows()<<std::endl;
+//test.print();
+//test.transponse();
+std::vector<int> as{2};
+std::vector<int> bs{2};
+//test.print();
+//test.delete_cr(as,bs);
+//test.print();
+test.check();
+test.status();
+while(0==0){
+std::cin>>i;
+std::cin>>j;
+std::cout<<test.cell(i,j)<<std::endl;
+
+}
+test.show_header();
+std::cin>>i;
+//cout<<sizeof(char)*all.size()<<" bytes"<<endl;
+}
